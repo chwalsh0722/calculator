3dc5cb70086fb476302019b516a85640480c4915
Cullen <lilspikeyc@gmail.com>
2023-04-03 17:31:12 -0500

    Basic computation logic complete. Working on number sign logic.

--- calculate.js
+++ calculate.js
@@ -22,27 +22,65 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
+function applyNumberSigns() {
+
+  // Set sign for numsParsed[0]
+  if (numSigns[0] === '+') {
+    numsParsed[0] = numsParsed[0]; 
+  }
+  else {
+    numsParsed[0] = -1 * numsParsed[0];
+  }
+
+  // Set sign for numParsed[1]
+  if (numSigns[1] === '+') {
+    numsParsed[1] = numsParsed[1];
+  } else {
+    numsParsed[1] = -1 * numsParsed[1];
+  }
+}
+
 function operate(num1, num2, operator)
 {
+  numsParsed = [parseInt(num1), parseInt(num2)];
+  applyNumberSigns();
+
   let calcResult;
   switch(operator) {
     case '+':
-      calcResult = add(num1, num2);
+      calcResult = add(numsParsed[0], numsParsed[1]);
       break;
     case '-':
-      calcResult = subtract(num1, num2);
+      calcResult = subtract(numsParsed[0], numsParsed[1]);
       break;
     case '*':
-      calcResult = multiplty(num1, num2);
+      calcResult = multiply(numsParsed[0], numsParsed[1]);
       break;
     case '/':
-      calcResult = divide(num1, num2);
+      calcResult = divide(numsParsed[0], numsParsed[1]);
   }
+  console.log(calcResult);
+  calcResult = updateSigns(calcResult);
+  nums[0] = String(calcResult);
+  nums[1] = '';
+  console.log("nums = ", nums);
+  console.log("numSigns = ", numSigns)
 }
 
 /*
   *** FUNCTIONS FOR CALCULATOR OPERATION ***
 */
+
+function updateSigns(calcResult) {
+  if (calcResult < 0) {
+    numSigns[0] = '-';
+    calcResult = -1 * calcResult;
+  } else {
+    numSigns[0] = '+';
+  }
+  numSigns[1] = '+';
+  return calcResult;
+}
 
 function getButtonClicked(button) {
   return button.id;
@@ -84,46 +122,92 @@ function getNumPressed(buttonID) {
     case 'num-nine':
       pressed = '9';
   }
-  return pressed;
+  nums[numUserEntering] += pressed;
+  console.log(nums);
 }
+
+// function parseProblemStringArray()
+// {
+//   let length = problemStringArray.length;
+//   let numString = '';
+//   let numsIndex = 0;
+//   let operator = '';
+//   for (let i = 0; i < length; i++)
+//   {
+//     let char = problemStringArray[i];
+//     if (char === '1' || char === '2' || char === '3' || char === '4' || char === '5' || char === '6' || char === '7' || char === '8' || char === '9' || char === '0') {
+//       numString += char;
+//     } else {
+//       nums[numsIndex] = parseInt(numString);
+//       operator = char;
+//       numString = '';
+//       numsIndex++;
+//     }
+//   }
+//   nums[1] = parseInt(numString);
+//   console.log(nums);
+//   console.log(operator);
+//   operate(nums[0], nums[1], operator);
+// }
 
 function getOperatorPressed(buttonID) {
   let pressed = '';
   switch(buttonID) {
     case 'operation-divide':
-      pressed = ' / ';
+      operator = "/";
       break;
     case 'operation-multiply':
-      pressed = ' * ';
+      operator = "*";
       break;
     case 'operation-minus':
-      pressed = ' - ';
+      operator = "-";
       break;
     case 'operation-plus':
-      pressed = ' + ';
+      operator = "+";
       break;
     case 'operation-clear':
-      pressed = 'c';
+      operator = "c";
+      break;
+    case 'operation-equals':
+      operate(nums[0], nums[1], operator);
+      operator = '';
+  }
+
+  console.log(operator);
+
+  // If C pressed, reset all global variables.
+  if (operator === 'c') {
+    nums = ['', ''];
+    numSigns = ['+', '+'];
+    operator = '';
+    numUserEntering = 0;
+    problemStringArray = [];
+    console.log('nums = ', nums);
+    console.log('numSigns = ', numSigns);
+    console.log('operator = ', operator);
+    console.log('numUserEntering = ', numUserEntering);
+    console.log('problemStringArray = ', problemStringArray);
   }
 }
 
-function checkIfClear(pressed) {
-  if (pressed = 'c') {
-    problemString = '';
+function changeNumSign() {
+  if (numSigns[numUserEntering] === '+') {
+    numSigns[numUserEntering] = '-';
+  } else if (numSigns[numUserEntering] === '-') {
+    numSigns[numUserEntering] = '+';
   }
+  console.log(numSigns);
 }
 
-function updateProblemString(buttonID, buttonType) {
-  // If number button pressed, append to 'problemString'.
-  let pressed = '';
+function updateGlobalVariables(buttonID, buttonType) {
+  // If number button pressed, append to 'problemStringArray'.
   if (buttonType === 'number') {
-    pressed = getNumPressed(buttonID);
-    problemString += pressed;
+    getNumPressed(buttonID);
   } else if (buttonType === 'operation') {
-    pressed = getOperatorPressed(buttonID);
-    
-    problemString += pressed;
+    getOperatorPressed(buttonID);
+    numUserEntering = 1;
   } else {
+    changeNumSign();
     // place/remove negative sign from front of number
     // currently being displayed.
     return;
@@ -145,14 +229,11 @@ function getButtonType(buttonID) {
 function buttonFunctions(e) {
   // Get ID of clicked element.
   let buttonID = getButtonClicked(this);
-  console.log(buttonID);
 
   // Get type of button that was clicked (number or operation).
   let buttonType = getButtonType(buttonID);
-  console.log(buttonType);
 
-  updateProblemString(buttonID, buttonType);
-  console.log(problemString);
+  updateGlobalVariables(buttonID, buttonType);
 
   // Update string used to for display.
 
@@ -163,11 +244,13 @@ function buttonFunctions(e) {
   
 }
 
-let num1 = '';
-let num2 = '';
-let mathResult = '';
+let nums = ['', ''];
+let numsParsed = [0, 0];
+let numSigns = ['+', '+'];
+let numUserEntering = 0;
+let numSign1 = '+';
+let numSign2 = '+';
 let operator = '';
-let problemString = '';
 
 
 const buttons = document.querySelectorAll('button');
